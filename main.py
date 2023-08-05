7820c733f960a736f7a8cc8f4789064ae8eb4be6
Jamie Martin <333jamie333@gmail.com>
2023-08-05 08:41:58 -0400

    Start of locking implementation
    Not done setting up locks. Going to bed. Do more later

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ import _thread, random
 #be loaded to run several things as State machine runs at 0.1MHz
 #and Pico core is at 125MHz
 #This allows for PWM output on unused core without interupting main, Core0
+newLock = _thread.allocate_lock()
 
 pins = [25, 28, 16, 17, 18, 19, 13,24 ]
 pinInt = [0,1,2,3,4,5,6,7]
 ground = 12
-pinV = [0, 0, 0, 255, 255, 255, 255, 255]
+pinV = [0, 0, 0, 65530, 65530, 65530, 65530, 65530]
 #random.randint()/1000 may generate and invalid decimal that will lock the core it running on
 pinFV= [100,120,110,130,111,120,110]
 pinDelay= [0.001,0.002,0.001,0.002,0.001,0.003,0.001]
@@ -19,6 +20,7 @@ fr = 0
 max_count = 65530
 pin = 0
 value = 0
+PWMLock = newLock
 pins[0] = PWM(Pin(25)) #G10
 pins[1] = PWM(Pin(28)) #G9
 pins[2] = PWM(Pin(16)) #G0
@@ -55,6 +57,7 @@ def pinFaderUp():
     global pinFV
     global pin
     global value
+    PWMLock.acquire()
     if (pinV[pin] < value):
         while (pinV[pin] < value):
             pinV[pin] = pinV[pin] + pinFV[pin]
@@ -62,12 +65,15 @@ def pinFaderUp():
                 pinV[pin] = 255
         pins[pin].duty_u16(pinV[pin])
         sleep(pinDelay[pin]/1000)
-def pinFaderDown()
+    PWMLock.release()
+
+def pinFaderDown():
     global pins
     global pinV
     global pinFV
     global pin
     global value    
+    PWMLock.acquire()
     if (pinV[pin] > value):
         while (pinV[pin] > value):
             pinV[pin] = pinV[pin] - pinFV[pin]
@@ -75,9 +81,9 @@ def pinFaderDown()
             pinV[pin] = 0
         pins[pin].duty_u16(pinV[pin])
         sleep(pinDelay[pin]/1000)
+    PWMLock.release()
 
 
-#core1 unable to use variables from main without passing them back and forth
 def core1_village_houses():
     print("hello")
 second_thread = _thread.start_new_thread(core1_village_houses, ())
@@ -87,18 +93,21 @@ def loop():
     global max_count
     global pinInt
     while True:
-        while fr < max_count:
-            fr = fr + 100
-            for x in pinInt:
-                pins[x].duty_u16(fr)
-                x = x + 1
-            sleep(sT)
-        while fr > 0:
-            fr = fr - 100
-            for x in pinInt:
-                pins[x].duty_u16(fr)
-                x = x + 1
-            sleep(sT)
+        PWMLock.acquire()
+        if PWMLock.locked():
+            while fr < max_count:
+                fr = fr + 100
+                for x in pinInt:
+                    pins[x].duty_u16(fr)
+                    x = x + 1
+                sleep(sT)
+            while fr > 0:
+                fr = fr - 100
+                for x in pinInt:
+                    pins[x].duty_u16(fr)
+                    x = x + 1
+                sleep(sT)
+            PWMLock.release()
 setup1()
 #loop()
 second_thread = _thread.start_new_thread(loop, ())
